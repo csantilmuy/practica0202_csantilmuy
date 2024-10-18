ca9ed0a3ec063f678cbe613858b918ac35680a06
csantilmuy <csantilmuy@educacion.navarra.es>
2024-10-18 21:39:27 +0200

    Update Ejercicio_08.py

--- Ejercicio_08.py
+++ Ejercicio_08.py
@@ -2,4 +2,4 @@
 # euros con dos decimales y muestre por pantalla el número de euros y el
 # número de céntimos del precio introducido.
 precio = input("Precio del producto en € con dos decimales: ")
-print(precio[:precio.find('.')], 'euros con', precio[precio.find('.')+1:], 'céntimos.')
+print(precio[:precio.find(".")], "euros con", precio[precio.find(".")+1:], "céntimos.")
